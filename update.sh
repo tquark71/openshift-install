4d41fec7ed6b9a10ede411454d2085553e07496c
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-18 16:38:50 -0200

    Create update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 yum -y update
 
+yum install -y epel-release
+
 yum install -y wget git perl net-tools docker-1.13.1 bind-utils iptables-services bridge-utils openssl-devel bash-completion kexec-tools sos psacct python-cryptography python2-pip python-devel python-passlib java-1.8.0-openjdk-headless "@Development Tools"
