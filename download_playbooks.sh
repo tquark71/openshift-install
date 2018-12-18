48e75b2ce8e211c2dc010a1707088273465e489a
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-18 17:19:05 -0200

    Create Download Playbooks

--- download_playbooks.sh
+++ download_playbooks.sh
@@ -1,1 +1,5 @@
-yum install -y python-passlib apache2-utils
+# yum install -y python-passlib apache2-utils
+
+git clone https://github.com/openshift/openshift-ansible.git
+cd openshift-ansible
+git checkout release-3.11
