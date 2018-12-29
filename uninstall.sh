82c0ea2efb1ba517f53358019b4ee7b398b29c53
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-29 09:32:35 -0200

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
 cd ~/openshift-ansible
-ansible-playbook -i openshift_inventory playbooks/adhoc/uninstall_openshift.yml 2>&1 | tee /tmp/uninstall.log
+ansible-playbook -i ../openshift-install/openshift_inventory playbooks/adhoc/uninstall_openshift.yml 2>&1 | tee /tmp/uninstall.log
