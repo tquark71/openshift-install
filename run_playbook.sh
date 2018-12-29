b7bec6204bd4f59e3ab6daa5d36b155c70fa4855
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-29 09:55:29 -0200

    Fix run playbooks

--- run_playbook.sh
+++ run_playbook.sh
@@ -1,3 +1,3 @@
 cd ~/openshift-ansible
 echo Running playbook $1
-ansible-playbook -i openshift_inventory $1 2>&1 | tee /tmp/run_playbook.log
+ansible-playbook -i ../openshift-install/openshift_inventory $1 2>&1 | tee /tmp/run_playbook.log
