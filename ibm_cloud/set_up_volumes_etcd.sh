d6962eaceaf38994751de0dd40c552ab09ba5949
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-31 13:45:09 -0200

    Define IBM Cloud set up volumes etcd

---
+++ ibm_cloud/set_up_volumes_etcd.sh
@@ -0,0 +1,11 @@
+INVENTORY=../openshift-install/openshift_inventory
+
+ansible -i $INVENTORY etcd -a 'yum -y install lvm2'
+#ansible -i $INVENTORY etcd -a 'wipefs /dev/sdc -a'
+ansible -i $INVENTORY etcd -a 'pvcreate -f /dev/xvde'
+ansible -i $INVENTORY etcd -a 'vgcreate etcd-vg /dev/xvde'
+ansible -i $INVENTORY etcd -a 'lvcreate -n etcd-lv -l 100%VG etcd-vg'
+ansible -i $INVENTORY etcd -a 'mkfs.xfs /dev/mapper/etcd--vg-etcd--lv'
+ansible -i $INVENTORY etcd -m shell -a 'mkdir /var/lib/etcd'
+ansible -i $INVENTORY etcd -m lineinfile -a 'path=/etc/fstab regexp=etcd line="/dev/mapper/etcd--vg-etcd--lv /var/lib/etcd xfs defaults 0 0"'
+ansible -i $INVENTORY etcd -m shell -a 'mount -a'
