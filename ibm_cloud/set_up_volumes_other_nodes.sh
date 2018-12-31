988c0d730a5af739cecccb0abd7b93a28e1d3921
Eduardo Patrocinio <eduardop@us.ibm.com>
2018-12-31 13:57:11 -0200

    Define IBM Cloud set up volumes other nodes

--- ibm_cloud/set_up_volumes_other_nodes.sh
+++ ibm_cloud/set_up_volumes_other_nodes.sh
@@ -1,9 +1,9 @@
 INVENTORY=./openshift_inventory
 
 ansible -i $INVENTORY nodes:\!etcd -a 'yum -y install lvm2'
-ansible -i $INVENTORY nodes:\!etcd -a 'pvcreate -f /dev/sdc'
-ansible -i $INVENTORY nodes:\!etcd -a 'wipefs -a /dev/sdc'
-ansible -i $INVENTORY nodes:\!etcd -a 'vgcreate origin-vg /dev/sdc'
+ansible -i $INVENTORY nodes:\!etcd -a 'pvcreate -f /dev/xvde'
+# ansible -i $INVENTORY nodes:\!etcd -a 'wipefs -a /dev/sdc'
+ansible -i $INVENTORY nodes:\!etcd -a 'vgcreate origin-vg /dev/xvde'
 ansible -i $INVENTORY nodes:\!etcd -a 'lvcreate -n origin-lv -l 100%VG origin-vg'
 ansible -i $INVENTORY nodes:\!etcd -a 'mkfs.xfs /dev/mapper/origin--vg-origin--lv'
 ansible -i $INVENTORY nodes:\!etcd -m shell -a 'mkdir /var/lib/origin'
