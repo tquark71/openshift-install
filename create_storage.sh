c1d7d19b439d21d5f45f9d25db479e0c7d983206
jkwong888 <jkwong@ca.ibm.com>
2019-01-15 12:32:33 -0500

    update inventory to use different DNS domain for apps

--- create_storage.sh
+++ create_storage.sh
@@ -1,3 +1,15 @@
-oc login -u system:admin -n default
-oc create -f nfs-pv.yaml
-oc create -f nfs-claim.yaml
+ansible masters -i ../openshift-install/openshift_inventory \
+  -m shell \
+  -a 'oc login -u system:admin -n default'
+
+ansible masters -i ../openshift-install/openshift_inventory \
+  -m copy \
+  -a 'src=nfs-pv.yaml dest=/tmp/nfs-pv.yaml'
+
+ansible masters -i ../openshift-install/openshift_inventory \
+  -m shell \
+  -a 'oc create -f /tmp/nfs-pv.yaml'
+
+ansible masters -i ../openshift-install/openshift_inventory \
+  -m shell \
+  -a 'oc get pv'
